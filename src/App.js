daaa849d365a53c5a00615a4ab7964edf3a30aa3
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-03-31 12:53:16 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import TodoListHeader from "./TodoListHeader";
+import TodoListFooter from "./TodoListFooter";
+import TodoListTasks from "./TodoListTasks";
+
+class App extends React.Component {
+    render = () => {
+        return (
+            <div className="App">
+                <div className="todoList">
+                    <TodoListHeader />
+                    <TodoListTasks />
+                    <TodoListFooter />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
